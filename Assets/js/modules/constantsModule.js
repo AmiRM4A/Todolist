e7d8b9a11752ea9cfd1b44d1599e6f5368d7ea3a
Amir <asgry255@gmail.com>
2024-03-19 18:57:26 +0330

    :bento: Add Constants Module

---
+++ Assets/js/modules/constantsModule.js
@@ -0,0 +1,7 @@
+const ICONS_PATH = 'Assets/img/icon/';
+const LOGOS_PATH = 'Assets/img/logo/';
+const HEADER_TYPE_DELAY = 100;
+const $ = document;
+const LOCAL_STORAGE_TASKS_KEY = 'tasks';
+
+export { ICONS_PATH, LOGOS_PATH, HEADER_TYPE_DELAY, $, LOCAL_STORAGE_TASKS_KEY }
